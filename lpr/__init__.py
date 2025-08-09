c5897998fa272571b8ea3843e1dd03ef5ec8b4ec
Noora68 <12031011@mail.sustech.edu.cn>
2025-08-10 07:50:35 +0800

    Basic information

---
+++ lpr/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+
+# Basic information of the package
+__version__ = "1.0.0"
+__author__ = "Feitong Dong"
+__date__ = "August 2025"
+__description__ = "Definition, training and evaluation of LPR model"
+
